05473793ea5558095162f9ea49fda0587023b3d4
PiotrekPK <piotrekwiatek2004@gmail.com>
2022-10-17 13:23:40 +0200

    lekcja7

---
+++ lekcja6/script.js
@@ -0,0 +1,25 @@
+const btns = [...document.querySelectorAll("button")]
+const select = document.querySelector("select")
+const pjs = document.getElementById("pjs")
+
+pjs.style.color = "red"
+
+btns.map((btn, index) => {
+    btn.addEventListener("click" , () => {
+        const value = document.querySelector("input").value
+        pjs.style.fontSize = value + "%"
+        const selectValue = select.value
+        if(selectValue === "prosty") {
+            pjs.style.fontStyle = "normal"
+        }else {
+            pjs.style.fontStyle = "Italic"
+        }
+        if(index === 0) {
+           pjs.style.color = "red"
+        }else if (index === 1) {
+            pjs.style.color = "green"
+        }else {
+            pjs.style.color = "blue"
+        }
+    })
+})
